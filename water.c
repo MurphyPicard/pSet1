fe0eeafe569b1497d301bbe878e6258970ffb861
Ara <ayaghsizian@gmail.com>
2017-10-25 20:28:25 +0000

    water passed 9 of 9 tests on check 50

--- water.c
+++ water.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <cs50.h>
 
+
 int main (void){
-    int minutes = get_int();
-    int timeee = minutes * 12;
-    printf(timeee);
+
+
+
+
+
+
+    int minutes;
+
+    do
+    {
+        printf("How long is your shower in minutes?\n");
+        minutes = get_int();
+    }
+    while ( minutes <= 0 );
+
+    int bottles = minutes * 12;
+
+    printf("You used %i bottles of water.\n", bottles);
+
+
+
+
+
+
 }
