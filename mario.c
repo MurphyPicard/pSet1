9e54d734a1bd2af19f092b31a2038b17d883b9db
murphypicard <ayaghsizian@gmail.com>
2017-10-25 21:27:38 +0000

    mario.c passed 10 of 10 tests on check50

---
+++ mario.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main (void){
+
+    int height;
+
+    do
+    {
+        printf("pick 0 - 23 to see Mario's pyramid, then press enter: ");
+        height = get_int();
+
+    }
+    while(height < 0 || height > 23);
+
+    for(int h = 0; h < height; h++){
+
+        for(int s = 0; s < height - ( h + 1) ;s++){
+            printf(" ");
+        }
+
+        for(int i = 0; i < 2 + h; i++){
+            printf("#");
+        }
+
+        printf("\n");
+    }//for h
+
+}//main
