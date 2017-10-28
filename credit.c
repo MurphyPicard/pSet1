48cdeb264b399de7aab09d7dfe0dd6e292ada700
murphypicard <ayaghsizian@gmail.com>
2017-10-28 16:51:27 +0000

    credit - thinking of looping through number backwards and increasing mod

--- credit.c
+++ credit.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 
 int main (void){
 
-    printf("credit \n");
+    printf("put in a cc number \n");
+
+    long int ccnum;
+    ccnum = get_long_long();
+
+
+    int nDigits = floor(log10(ccnum)) + 1;
+
+    int sum = 0;
+    int mod = 10;
+    int divisor = 10;
+
+    for(int i = 0; i < nDigits; i++){
+
+
+        printf("hi\n");
+    }
+
+
+
+    //for(int i = 0; i < ){}
+
+
+    printf("%ld \n", ccnum);
+
+    printf("%i  \n", nDigits);
+
 
 
 
