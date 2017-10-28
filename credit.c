bd2fa706e72097856f9a1d58b2c208bdeb5c98c8
murphypicard <ayaghsizian@gmail.com>
2017-10-28 17:15:52 +0000

    trying an untested switch statement - this is the before

--- credit.c
+++ credit.c
@@ -14,24 +14,48 @@ int main (void){
     int nDigits = floor(log10(ccnum)) + 1;
 
     int sum = 0;
-    int mod = 10;
-    int divisor = 10;
-
-    for(int i = 0; i < nDigits; i++){
 
 
-        printf("hi\n");
+    int mod = 10; // mod starts at ten for this particular loop
+    for(int i = 0; i < nDigits; i += 2){
+
+        sum += (ccnum % mod) / (mod/10);
+
+        mod = mod * 100;
+
+        printf("sum: %i \n", sum);
     }
 
+    mod = 100; // mod starts at 100 for this particular loop
+    for(int j = 0; j < nDigits; j += 2){
 
+        switch(     (ccnum % mod) / (mod/10)    ) {
 
-    //for(int i = 0; i < ){}
+            case 1 :
+                sum += (ccnum % mod) / (mod/10);
+                break;
+
+            case 2 :
+                sum += (ccnum % mod) / (mod/10);
+                break;
+
+            default :
+            sum += (ccnum % mod) / (mod/10);
+        }//switch
+
+        //sum += (ccnum % mod) / (mod/10);
+
+        mod = mod * 100;
+
+        printf("sum: %i \n", sum);
+
+    }// for
+
 
 
     printf("%ld \n", ccnum);
 
     printf("%i  \n", nDigits);
-
 
 
 
