63a15f926551509c0d3add17dabd30c33e7c4631
murphypicard <ayaghsizian@gmail.com>
2017-10-28 14:37:03 +0000

    solved greedy and I love my solution on line 15 for edge cases

--- greedy.c
+++ greedy.c
@@ -4,7 +4,44 @@
 
 int main (void){
 
-    printf("greed is good\n");
-    // greedy test2
+    printf("how much due? ");
+    // greedy test3
+    double due;
+    due = get_float();
+
+
+    // to prevent computer rounding errors (not every number possible in binary)
+    // this ensures the due variable is always slightly above the real life amount owed
+    due += .001;
+
+    int coins = 0;
+
+    // give the Quarters back first
+    while(due >= .25){
+        coins++;
+        due-=.25;
+    }
+
+    // second give the dimes back
+    while(due >= .1){
+        coins++;
+        due-=.1;
+    }
+
+    // third give the nickels back
+    while(due >= .05){
+        coins++;
+        due-=.05;
+    }
+
+    // last give the pennies back
+    while(due >= .01){
+        coins++;
+        due-=.01;
+    }
+
+    // mininum number of coins returned
+    printf("%i \n", coins);
+
 
 }//main
