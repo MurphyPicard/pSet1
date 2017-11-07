64fcb30dc0d5d88152ea6720532bd7cfb5c97d1f
murphypicard <ayaghsizian@gmail.com>
2017-11-07 01:07:07 +0000

    mario MORE passed style50 yay

--- more/mario.c
+++ more/mario.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 #include <cs50.h>
 
-
-int main (void){
+int main (void)
+{
 
     int height;
 
+    // user picks a height from 0 - 23
     do
     {
         printf("pick 0 - 23 to see Mario's pyramid, then press enter: ");
         height = get_int();
 
     }
-    while(height < 0 || height > 23);
+    while (height < 0 || height > 23);
 
-    for(int h = 0; h < height; h++){
+    // printing spaces then hashes then a space then reciprocal hashes
+    for (int h = 0; h < height; h++)
+    {
 
-        for(int s = 0; s < height - ( h + 1) ;s++){
+        for (int s = 0; s < height - ( h + 1) ; s++)
+        {
             printf(" ");
         }
 
-        for(int i = 0; i < 1 + h; i++){
+        for (int i = 0; i < 1 + h; i++)
+        {
             printf("#");
         }
 
         printf("  ");
 
-        for(int i = 0; i < 1 + h; i++){
+        for (int i = 0; i < 1 + h; i++)
+        {
             printf("#");
         }
 
