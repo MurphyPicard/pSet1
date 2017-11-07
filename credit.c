c43ad9f5f6eac8b83780a640099ee04bbaa76729
murphypicard <ayaghsizian@gmail.com>
2017-11-07 01:13:45 +0000

    passed style50 with flying stylish colors

--- credit.c
+++ credit.c
@@ -2,8 +2,8 @@
 #include <cs50.h>
 #include <math.h>
 
-
-int main (void){
+int main (void)
+{
 
     printf("put in a cc number \n");
 
@@ -17,83 +17,72 @@ int main (void){
 
 
     long int mod = 10; // mod starts at ten for this particular loop
-    for(int i = 0; i < nDigits; i += 2){
+    for (int i = 0; i < nDigits; i += 2)
+    {
 
-        sum += (ccnum % mod) / (mod/10);
-
+        sum += (ccnum % mod) / (mod / 10);
         mod = mod * 100;
-
-        //printf("sum: %i \n", sum);
     }
 
-    mod = 100; // mod starts at 100 for this particular loop
-    for(int j = 0; j < nDigits; j += 2){
+    // mod starts at 100 for this particular loop
+    mod = 100;
+    for (int j = 0; j < nDigits; j += 2)
+    {
 
-        switch(     (ccnum % mod) / (mod/10)    ) {
-
+        switch ( (ccnum % mod) / (mod / 10) )
+        {
             case 9 :
                 sum += 9;
                 break;
-
             case 8 :
                 sum += 7;
                 break;
-
             case 7 :
                 sum += 5;
                 break;
-
             case 6 :
                 sum += 3;
                 break;
             case 5 :
                 sum += 1;
                 break;
-
             default :
-                sum += ( (ccnum % mod) / (mod/10) ) * 2;
+                sum += ( (ccnum % mod) / (mod / 10) ) * 2;
         }//switch
-
-        //sum += (ccnum % mod) / (mod/10);
 
         mod = mod * 100;
 
-        //printf("sum: %i \n", sum);
-
     }// for
 
-    //printf("%ld \n", ccnum);
-
-    //printf("%i  \n", nDigits);
-
-
-
-    if( sum % 10 == 0 && nDigits == 15 ){
+    if ( sum % 10 == 0 && nDigits == 15 )
+    {
         printf("AMEX\n");
     }
-    else if( sum % 10 == 0 && nDigits == 13 ){
+    else if ( sum % 10 == 0 && nDigits == 13 )
+    {
         printf("VISA\n");
     }
-    else if(sum % 10 == 0 && nDigits == 16 ){
+    else if (sum % 10 == 0 && nDigits == 16 )
+    {
 
-        if(ccnum / 1000000000000000 == 4){
+        if (ccnum / 1000000000000000 == 4)
+        {
             printf("VISA\n");
-
         }
-        else if(ccnum / 1000000000000000 == 5){
+        else if (ccnum / 1000000000000000 == 5)
+        {
             printf("MASTERCARD\n");
-
         }
-        else{
+        else
+        {
             printf("INVALID\n");
         }
-
     }
-    else{
+    else
+    {
         printf("INVALID\n");
     }
 
 // although I passed all tests on check50, I did not expect to YET and believe there are more tests needed to be thorough
 // that being said, I have other things to work on and will move on to them and be happy with the win!
-
 }//main
